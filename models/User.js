2b77ad176cfcd799237b05ac539dffa5a9b7ba4d
Damon Chhun <damonchhun7@gmail.com>
2020-08-17 13:27:00 -0700

    user model minor fix

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: TextTrackCue,
+    unique: true,
     passowrd: {
       type: String,
       required: true
