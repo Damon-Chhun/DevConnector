1b92666056e759f9684a463b521c55d1efd6fd7e
Damon Chhun <damonchhun7@gmail.com>
2020-08-19 19:07:23 -0700

    JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
+
+//User Model being imported
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
 router.post(
   "/",
+  //validation and error message
   [
     check("name", "Name is required")
       .not()
@@ -17,12 +25,64 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Users route");
+
+    // grabbing user inputs from req.body
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      // Get users gravatar ( pass in email and pass options )
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      //create new instance of User
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      // Return Jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (e) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
   }
 );
 
