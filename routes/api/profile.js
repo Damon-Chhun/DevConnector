716ca4761d1761f77d62957007e2f614609e6d71
Damon Chhun <damonchhun7@gmail.com>
2020-08-22 13:37:51 -0700

    Delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,22 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETEapi/profile
+// @desc    Delete profile, user & posts
+// @access  private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // todo - Remove users posts
+
+    // Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "user deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
