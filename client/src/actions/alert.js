804c57a27b3e72d60499fa14b83db9852f809dc7
Damon Chhun <damonchhun7@gmail.com>
2020-08-29 11:00:40 -0700

    add and remove alert actions

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,9 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
+import uuid from "uuid/v4";
 
 export const setAlert = (msg, alertType) => dispatch => {
-  const id = uuid.v4();
+  const id = uuid();
   dispatch({
     type: SET_ALERT,
     data: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, data: id }), 5000);
 };
